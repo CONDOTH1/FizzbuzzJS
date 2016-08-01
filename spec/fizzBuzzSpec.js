44f284d35d1377988a7ec0aa1df5ab0f546ec6ef
Thady Condon <thadycondon@hotmail.com>
2016-08-01 17:13:37 +0100

    Completed fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,5 +1,29 @@
-describe('#fizz', function() {
+describe('#fizzbuzz', function() {
   it('should return fizz when 3', function() {
-    expect(fizz(3)).toEqual('Fizz');
+    expect(fizzbuzz(3)).toEqual('Fizz');
+  });
+
+  it('should return fizz when 6', function() {
+    expect(fizzbuzz(6)).toEqual('Fizz');
+  });
+});
+
+describe('#buzz', function() {
+  it('should return buzz when 5', function() {
+    expect(fizzbuzz(5)).toEqual('Buzz');
+  });
+
+  it('should return buzz when 10', function() {
+    expect(fizzbuzz(10)).toEqual('Buzz');
+  });
+});
+
+describe('#Fizzbuzz', function() {
+  it('should return Fizzbuzz when 15', function() {
+    expect(fizzbuzz(15)).toEqual('Fizzbuzz');
+  });
+
+  it('should return Fizzbuzz when 30', function() {
+    expect(fizzbuzz(30)).toEqual('Fizzbuzz');
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,26 @@
 function fizz(num) {
-  if (num === 3) {
+  if (num % 3 === 0) {
     return 'Fizz'
   };
+};
+
+function buzz(num) {
+  if (num % 5 === 0) {
+    return 'Buzz'
+  };
+};
+
+function fizzbuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'Fizzbuzz';
+  }
+  else if (num % 5 == 0) {
+    return buzz(num)
+  }
+  else if (num % 3 == 0) {
+    return fizz(num)
+  }
+  else {
+    return num;
+  }
 };
