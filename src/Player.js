9cefdbe0000dc3fc0e4afe272d6981f37cc93cb4
Thady Condon <thadycondon@hotmail.com>
2016-08-01 16:48:54 +0100

    First commit

---
+++ src/Player.js
@@ -0,0 +1,18 @@
+function Player() {
+}
+Player.prototype.play = function(song) {
+  this.currentlyPlayingSong = song;
+  this.isPlaying = true;
+};
+
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
